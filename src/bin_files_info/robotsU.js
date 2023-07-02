cf281b78859c855ded1a0b5abc3a8418e9ec2851
Sigil <Sigil@OVERSEER-RX-I>
2023-07-01 20:13:58 -0400

    Minor hex adjustments

--- src/bin_files_info/robotsU.js
+++ src/bin_files_info/robotsU.js
@@ -5,13 +5,14 @@ const decompressedFileSizeInBytes = 144_748_383;
 const replacements = [
     //Subtypes
     { offset: 0x00638BB2, from: [0x05], to: [0x02] }, //Change MechSubtype0 ability to Ballistic Barrage
+    { offset: 0x0063987C, from: [0x01], to: [0x03] }, //Change MechSubtype2 secondary to Grenade Launcher
     { offset: 0x006391F0, from: [0x01], to: [0x05] }, //Change MechSubtype4 secondary to Repair Torch    
     { offset: 0x006396DE, from: [0x05], to: [0x03] }, //Change MechSubtype7 ability to Weapons Coolant
     
     //Items
     { offset: 0x0054e573, from: [0xC0, 0x40], to: [0xD0, 0x40] }, //Increase MG turret damage to 6.5
-    { offset: 0x0054e58D, from: [0x2C, 0x01], to: [0x43, 0x46] }, //Decrease Turret initial Health to 198
-    { offset: 0x0054e5A9, from: [0x2C, 0x01], to: [0x43, 0x46] }, //Decrease Turret max Health to 198    
+    { offset: 0x0054e58D, from: [0x2C, 0x01], to: [0xC6, 0x00] }, //Decrease Turret initial Health to 198
+    { offset: 0x0054e5A9, from: [0x2C, 0x01], to: [0xC6, 0x00] }, //Decrease Turret max Health to 198    
     { offset: 0x0054E6D2, from: [0x98, 0x3A], to: [0x2C, 0xEC] }, //Decrease Turret MaxTargetDistance to 11500
 
     { offset: 0x0055530A, from: [0x58, 0x02], to: [0x3F, 0x02] }, //Change Blockade Health to 575
@@ -33,7 +34,7 @@ const replacements = [
     { offset: 0x00687763, from: [0x5E, 0x01], to: [0xC2, 0x01] }, //Change Shield HP to 450
 
     //Mech Properties    
-    { offset: 0x006690B9, from: [0x00, 0x00, 0xAF, 0x43], to: [0x00, 0x00, 0x2F, 0x44] }, //Increase R_Pawn ThrusterVerticalSpeed to 700
+    { offset: 0x006690B7, from: [0x00, 0x00, 0xAF, 0x43], to: [0x00, 0x00, 0x2F, 0x44] }, //Increase R_Pawn ThrusterVerticalSpeed to 700
     { offset: 0x0066AD9F, from: [0x00, 0x00, 0xD2, 0x43], to: [0x00, 0x80, 0x9D, 0x44] } //Increase R_Pawn AccelRate to 1260
 ];
 
